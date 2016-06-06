8a8b6345d1c914de46401e534d3e9fc30c3ab003
Prem Suman <Prem Suman>
2016-06-06 15:50:36 +0530

    latest LC

---
+++ app/src/main/java/com/lovecalculator/prem/lovecalculator/MainActivity.java
@@ -0,0 +1,85 @@
+package com.lovecalculator.prem.lovecalculator;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText self_edit;
+    EditText partner_edit;
+    RadioButton selfM;
+    RadioButton selfF;
+    RadioButton partnerM;
+    RadioButton partnerF;
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        self_edit = (EditText) findViewById(R.id.self_editText);
+        partner_edit = (EditText) findViewById(R.id.partner_editText);
+        selfM = (RadioButton) findViewById(R.id.self_radioButtonM);
+        selfF = (RadioButton) findViewById(R.id.self_radioButtonF);
+        partnerM = (RadioButton) findViewById(R.id.partner_radioButtonM);
+        partnerF = (RadioButton) findViewById(R.id.partner_radioButtonF);
+        b = (Button) findViewById(R.id.button1);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.self_radioButtonM:
+                if (checked) {
+                    selfM.setTypeface(Typeface.DEFAULT_BOLD);
+                    selfF.setTypeface(Typeface.DEFAULT);
+                    selfM.setTextColor(Color.parseColor("#26c076"));
+                    selfF.setTextColor(Color.parseColor("#d41a1d"));
+                    break;
+                }
+            case R.id.self_radioButtonF:
+                if (checked) {
+                    selfM.setTypeface(Typeface.DEFAULT);
+                    selfF.setTypeface(Typeface.DEFAULT_BOLD);
+                    selfM.setTextColor(Color.parseColor("#d41a1d"));
+                    selfF.setTextColor(Color.parseColor("#26c076"));
+                    break;
+                }
+            case R.id.partner_radioButtonM:
+                if (checked) {
+                    partnerM.setTypeface(Typeface.DEFAULT_BOLD);
+                    partnerF.setTypeface(Typeface.DEFAULT);
+                    partnerM.setTextColor(Color.parseColor("#26c076"));
+                    partnerF.setTextColor(Color.parseColor("#d41a1d"));
+                    break;
+                }
+            case R.id.partner_radioButtonF:
+                if (checked) {
+                    partnerM.setTypeface(Typeface.DEFAULT);
+                    partnerF.setTypeface(Typeface.DEFAULT_BOLD);
+                    partnerM.setTextColor(Color.parseColor("#d41a1d"));
+                    partnerF.setTextColor(Color.parseColor("#26c076"));
+                    break;
+                }
+        }
+    }
+}
